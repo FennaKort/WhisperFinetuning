e7a6f1a82d4aa67023ab3da9a23051c2bd0df66b
Fenna Kort <fenna.kort@tutanota.com>
2026-08-12 09:57:40 -0600

    updated to include functools partial and to update audio file names used for demos

--- demosnippets.py
+++ demosnippets.py
@@ -4,6 +4,7 @@ from whisper import load_audio, pad_or_trim, load_model, transcribe
 from transformers import AutoProcessor, WhisperForConditionalGeneration, WhisperProcessor, WhisperTokenizer
 from evaluate import load
 from datasets import Audio, Dataset
+from functools import partial
 
 from finetune import FineTuner, extract_features_and_tokenize
 
@@ -25,29 +26,29 @@ def demo_load_audio_output():
 	
 	Shows that the amplitude arrays are shorter when the function resamples audio at Whisper's native sampling rate of 16kHz, and that Whisper will automatically resample to this rate if no sampling rate is passed into load_audio(). Using this process therefore replicates the functionality of `datasets` `cast_column("audio", Audio(sampling_rate=16000))` method, which is the resampling approach recommended in most Whisper fine-tuning guides that use premade audio datasets that already have amplitude array data constructed from their audio files' native sampling rates. 
 	"""
-	audio_array = load_audio("res/audio/voice-message-1.mp3", sr=44100) #44.1kHz is a typical native sampling rate for audio, so I am using sr (samplingrate) = 44100 for demonstration purposes
+	audio_array = load_audio("res/audio/voice-message-01.mp3", sr=44100) #44.1kHz is a typical native sampling rate for audio, so I am using sr (samplingrate) = 44100 for demonstration purposes
 	print(f"{len(audio_array)} amplitude samples when sampled at 44.1kHz") 
 	# x samples, 1632384 samples when using demo audio "res/audio/voice-message-1.mp3"
 
-	audio_array = load_audio("res/audio/voice-message-1.mp3", sr=16000) # Whisper internally operates at 16kHz
+	audio_array = load_audio("res/audio/voice-message-01.mp3", sr=16000) # Whisper internally operates at 16kHz
 	print(f"{len(audio_array)} amplitude samples when sampled at 16kHz") 
 	#y samples, 592248 samples when using demo audio "res/audio/voice-message-1.mp3"
 	# y samples = x samples/2.75625 (because 44100/16000=2.75625)
 
-	audio_array = load_audio("res/audio/voice-message-1.mp3") # if no sampling rate is provided, Whisper will use its default sampling rate of 16000/16kHz.
+	audio_array = load_audio("res/audio/voice-message-01.mp3") # if no sampling rate is provided, Whisper will use its default sampling rate of 16000/16kHz.
 	print(f"{len(audio_array)} amplitude samples when sampled at default Whisper sample rate") 
 	# z samples, same number of samples as y
 	#592248 samples
 
 def demo_pad_array():
-	audio_array = load_audio("res/audio/voice-message-1.mp3") # if no sampling rate is provided, Whisper will load audio using its default sampling rate of 16000/16kHz.
+	audio_array = load_audio("res/audio/voice-message-01.mp3") # if no sampling rate is provided, Whisper will load audio using its default sampling rate of 16000/16kHz.
 	print(f"{len(audio_array)} amplitude samples when sampled at default Whisper sample rate") 
 	#592248 samples
 
 	audio_array = pad_or_trim(audio_array)
 	print(f"{len(audio_array)} samples after trimming") # audio is longer than 30s/480000 samples, will be trimmed to 480000 samples, 16kHz sample rate * 30 second chunk size
 
-	audio_array = load_audio("res/audio/voice-message-4.ogg") # if no sampling rate is provided, Whisper will use its default sampling rate of 16000/16kHz.
+	audio_array = load_audio("res/audio/voice-message-04.ogg") # if no sampling rate is provided, Whisper will use its default sampling rate of 16000/16kHz.
 	print(f"{len(audio_array)} amplitude samples when sampled at default Whisper sample rate") 
 	#158400 samples
 
@@ -91,7 +92,7 @@ def demo_compare_audio_data_creation_methods():
 		dataset = Dataset.from_json("res/validated-audio/metadata-subset.json")
 		dataset = dataset.rename_column("file_name","audio").cast_column("audio", Audio(sampling_rate=16000))
 
-		dataset_mapped = dataset.map(extract_features_and_tokenize, remove_columns=["speech_ends_at","model_name","manually_verified"])
+		dataset_mapped = dataset.map(partial(extract_features_and_tokenize, processor=processor), remove_columns=["audio","transcript","speech_ends_at","model_name","manually_verified"], num_proc=4)
 		
 		return dataset_mapped
 
